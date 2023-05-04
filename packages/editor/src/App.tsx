e70f015df914eae0cbc7fbb8b3c4141948a1c5a5
Ryohei Ikegami <iofg2100@gmail.com>
2023-05-04 21:11:48 +0900

    Stub wrapper UI

--- packages/editor/src/App.tsx
+++ packages/editor/src/App.tsx
@@ -1,12 +1,24 @@
 import { observer } from "mobx-react-lite";
 import { editorState } from "./EditorState";
 import { Renderer } from "./Renderer";
+import * as path from "path-browserify";
 
 const App: React.FC = observer(() => {
   return (
     <main>
       <div>{editorState.tabPath}</div>
-      <Renderer />
+      <div className="m-8">
+        <h2 className="font-semibold mb-1">
+          {editorState.tabPath &&
+            path.basename(
+              editorState.tabPath,
+              path.extname(editorState.tabPath)
+            )}
+        </h2>
+        <div className="w-[1024px] bg-white">
+          <Renderer />
+        </div>
+      </div>
     </main>
   );
 });
