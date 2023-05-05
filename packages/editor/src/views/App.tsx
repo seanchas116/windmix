8ad72212a5d226aaf71a5fa97cb098c37aee3f5b
Ryohei Ikegami <iofg2100@gmail.com>
2023-05-05 15:37:30 +0900

    Tweak inspector width

--- packages/editor/src/views/App.tsx
+++ packages/editor/src/views/App.tsx
@@ -23,7 +23,7 @@ const App: React.FC = observer(() => {
           </div>
         </div>
       </div>
-      <aside className="w-[256px] bg-white/5 border-l border-white/5">
+      <aside className="w-[224px] bg-white/5 border-l border-white/5">
         <PaintkitRoot colorScheme="dark">
           <StyleInspector state={appState.styleInspectorState} />
         </PaintkitRoot>
