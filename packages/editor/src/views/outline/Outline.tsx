7176c1d8344b5b425cf3fcf88a7a87efa1b91a98
Ryohei Ikegami <iofg2100@gmail.com>
2023-05-06 14:43:32 +0900

    Refactor

---
+++ packages/editor/src/views/outline/Outline.tsx
@@ -0,0 +1,57 @@
+import { observer } from "mobx-react-lite";
+import { NodeRendererProps, Tree } from "react-arborist";
+import { FillFlexParent } from "../../components/FillFlexParent";
+
+function Node({ node, style, dragHandle }: NodeRendererProps<any>) {
+  /* This node instance can do many things. See the API reference. */
+  return (
+    <div style={style} ref={dragHandle} onClick={() => node.toggle()}>
+      {node.isLeaf ? " " : node.isOpen ? "-" : "+"} {node.data.name}
+    </div>
+  );
+}
+
+const data = [
+  { id: "1", name: "Unread" },
+  { id: "2", name: "Threads" },
+  {
+    id: "3",
+    name: "Chat Rooms",
+    children: [
+      { id: "c1", name: "General" },
+      { id: "c2", name: "Random" },
+      { id: "c3", name: "Open Source Projects" },
+    ],
+  },
+  {
+    id: "4",
+    name: "Direct Messages",
+    children: [
+      { id: "d1", name: "Alice" },
+      { id: "d2", name: "Bob" },
+      { id: "d3", name: "Charlie" },
+    ],
+  },
+];
+
+export const Outline: React.FC = observer(() => {
+  return (
+    <FillFlexParent>
+      {({ width, height }) => (
+        <Tree
+          initialData={data}
+          openByDefault={false}
+          width={width}
+          height={height}
+          indent={16}
+          rowHeight={32}
+          paddingTop={12}
+          paddingBottom={12}
+          padding={12}
+        >
+          {Node}
+        </Tree>
+      )}
+    </FillFlexParent>
+  );
+});
