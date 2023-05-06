75627c3dad4ea7c1a5b1befe77a289096cc5b7cc
Ryohei Ikegami <iofg2100@gmail.com>
2023-05-07 00:09:35 +0900

    contain:strict

--- packages/editor/src/views/App.tsx
+++ packages/editor/src/views/App.tsx
@@ -14,10 +14,10 @@ const App: React.FC = observer(() => {
     fixed inset-0 flex text-xs
     "
     >
-      <aside className="w-[224px] h-full bg-white/5 border-r border-white/5 relative">
+      <aside className="w-[224px] h-full bg-white/5 border-r border-white/5 relative contain-strict">
         <Outline />
       </aside>
-      <main className="flex-1 min-w-0">
+      <main className="flex-1 min-w-0 contain-strict">
         <div className="m-4">
           <h2 className="font-semibold mb-1">
             {appState.tabPath &&
@@ -28,7 +28,7 @@ const App: React.FC = observer(() => {
           </div>
         </div>
       </main>
-      <aside className="w-[224px] h-full bg-white/5 border-l border-white/5 relative">
+      <aside className="w-[224px] h-full bg-white/5 border-l border-white/5 relative contain-strict">
         <ScrollArea>
           <PaintkitRoot colorScheme="dark">
             <StyleInspector state={appState.styleInspectorState} />
