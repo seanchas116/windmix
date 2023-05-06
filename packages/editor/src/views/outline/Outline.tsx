77db45c42bda05b77be1f4ac3812d6f43169e04b
Ryohei Ikegami <iofg2100@gmail.com>
2023-05-06 17:09:54 +0900

    Show tree

--- packages/editor/src/views/outline/Outline.tsx
+++ packages/editor/src/views/outline/Outline.tsx
@@ -2,69 +2,86 @@ import { observer } from "mobx-react-lite";
 import { NodeRendererProps, Tree } from "react-arborist";
 import { FillFlexParent } from "../../components/FillFlexParent";
 import { Icon } from "@iconify/react";
+import { Node } from "@windmix/model";
+import { appState } from "../../state/AppState";
 
-function Node({ node, style, dragHandle }: NodeRendererProps<any>) {
+function NodeRow({
+  node: treeNode,
+  style,
+  dragHandle,
+}: NodeRendererProps<Node>) {
   /* This node instance can do many things. See the API reference. */
+  const node = treeNode.data;
+
+  const getName = () => {
+    switch (node.type) {
+      case "component":
+        return node.name;
+      case "element":
+        return node.data.get("tagName");
+      case "text":
+        return node.data.get("text");
+      case "expression":
+        return node.data.get("code");
+      case "wrappingExpression":
+        return node.data.get("header");
+      default:
+        return;
+    }
+  };
+
+  const getIcon = () => {
+    switch (node.type) {
+      case "component":
+        return <Icon icon="icon-park-outline:figma-component" />;
+      case "element":
+        return <Icon icon="icon-park-outline:code" />;
+      case "text":
+        return <Icon icon="icon-park-outline:font-size" />;
+      case "expression":
+      case "wrappingExpression":
+        return <Icon icon="icon-park-outline:code-brackets" />;
+      default:
+    }
+  };
+
   return (
     <div
       style={style}
       ref={dragHandle}
-      onClick={() => node.toggle()}
-      className="h-full"
+      onClick={() => treeNode.toggle()}
+      className="h-full hover:ring-1 hover:ring-inset hover:ring-blue-500"
     >
-      <div className="flex items-center h-full pl-1 gap-1">
-        {node.isLeaf ? (
+      <div className="flex items-center h-full pl-1 gap-2">
+        {node.type === "text" ? (
           <div className="w-[1em] h-[1em]" />
-        ) : node.isOpen ? (
+        ) : treeNode.isOpen ? (
           <Icon icon="material-symbols:chevron-right" rotate={1} />
         ) : (
           <Icon icon="material-symbols:chevron-right" />
         )}
-        {node.data.name}
+        <span className="opacity-50">{getIcon()}</span>
+        <span className="min-w-0 truncate">{getName()}</span>
       </div>
     </div>
   );
 }
-
-const data = [
-  { id: "1", name: "Unread" },
-  { id: "2", name: "Threads" },
-  {
-    id: "3",
-    name: "Chat Rooms",
-    children: [
-      { id: "c1", name: "General" },
-      { id: "c2", name: "Random" },
-      { id: "c3", name: "Open Source Projects" },
-    ],
-  },
-  {
-    id: "4",
-    name: "Direct Messages",
-    children: [
-      { id: "d1", name: "Alice" },
-      { id: "d2", name: "Bob" },
-      { id: "d3", name: "Charlie" },
-    ],
-  },
-];
 
 export const Outline: React.FC = observer(() => {
   return (
     <FillFlexParent>
       {({ width, height }) => (
         <Tree
-          initialData={data}
+          data={appState.fileNode.children}
           openByDefault={false}
           width={width}
           height={height}
-          indent={12}
+          indent={8}
           rowHeight={24}
           paddingTop={12}
           paddingBottom={12}
-          padding={12}
         >
-          {Node}
+          {NodeRow}
         </Tree>
       )}
     </FillFlexParent>
