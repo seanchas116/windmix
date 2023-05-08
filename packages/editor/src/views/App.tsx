b9294cd6962a6aad1ccfa31701bd3a57d2b13747
Ryohei Ikegami <iofg2100@gmail.com>
2023-05-08 13:01:21 +0900

    Add Viewport component

--- packages/editor/src/views/App.tsx
+++ packages/editor/src/views/App.tsx
@@ -1,11 +1,10 @@
 import { observer } from "mobx-react-lite";
 import { appState } from "../state/AppState";
-import { Renderer } from "./Renderer";
-import * as path from "path-browserify";
 import { StyleInspector } from "./inspector/StyleInspector";
 import { PaintkitRoot } from "@seanchas116/paintkit/src/components/PaintkitRoot";
 import { ScrollArea } from "@seanchas116/paintkit/src/components/ScrollArea";
 import { Outline } from "./outline/Outline";
+import { Viewport } from "./Viewport";
 
 const App: React.FC = observer(() => {
   return (
@@ -17,17 +16,7 @@ const App: React.FC = observer(() => {
       <aside className="w-[224px] h-full bg-white/5 border-r border-white/5 relative contain-strict">
         <Outline />
       </aside>
-      <main className="flex-1 min-w-0 contain-strict">
-        <div className="m-4">
-          <h2 className="font-semibold mb-1">
-            {appState.tabPath &&
-              path.basename(appState.tabPath, path.extname(appState.tabPath))}
-          </h2>
-          <div className="w-[1024px] bg-white">
-            <Renderer />
-          </div>
-        </div>
-      </main>
+      <Viewport />
       <aside className="w-[224px] h-full bg-white/5 border-l border-white/5 relative contain-strict">
         <ScrollArea>
           <PaintkitRoot colorScheme="dark">
