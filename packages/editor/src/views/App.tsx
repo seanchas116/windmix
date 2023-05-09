98196818f6237f6fc5d99cf9376ef9dc66a3694b
Ryohei Ikegami <iofg2100@gmail.com>
2023-05-09 23:38:11 +0900

    Fix

--- packages/editor/src/views/App.tsx
+++ packages/editor/src/views/App.tsx
@@ -8,23 +8,23 @@ import { Viewport } from "./viewport/Viewport";
 
 const App: React.FC = observer(() => {
   return (
-    <div
-      className="
+    <PaintkitRoot colorScheme="dark">
+      <div
+        className="
     fixed inset-0 flex text-xs
     "
-    >
-      <aside className="w-[224px] h-full bg-white/5 border-r border-white/5 relative contain-strict">
-        <Outline />
-      </aside>
-      <Viewport />
-      <aside className="w-[224px] h-full bg-white/5 border-l border-white/5 relative">
-        <ScrollArea>
-          <PaintkitRoot colorScheme="dark">
+      >
+        <aside className="w-[224px] h-full bg-white/5 border-r border-white/5 relative contain-strict">
+          <Outline />
+        </aside>
+        <Viewport />
+        <aside className="w-[224px] h-full bg-white/5 border-l border-white/5 relative">
+          <ScrollArea>
             <StyleInspector state={appState.styleInspectorState} />
-          </PaintkitRoot>
-        </ScrollArea>
-      </aside>
-    </div>
+          </ScrollArea>
+        </aside>
+      </div>
+    </PaintkitRoot>
   );
 });
 
