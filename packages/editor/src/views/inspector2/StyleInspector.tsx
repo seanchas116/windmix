60515ad9c132dfcf2e49759c849152c9a839ffa3
Ryohei Ikegami <iofg2100@gmail.com>
2023-05-11 23:48:56 +0900

    Show inputs

--- packages/editor/src/views/inspector2/StyleInspector.tsx
+++ packages/editor/src/views/inspector2/StyleInspector.tsx
@@ -2,6 +2,15 @@ import { observer } from "mobx-react-lite";
 import { appState } from "../../state/AppState";
 import { sameOrNone } from "@seanchas116/paintkit/src/util/Collection";
 import { TailwindValue } from "../../models/style2/TailwindStyle";
+import {
+  Pane,
+  PaneHeading,
+  PaneHeadingRow,
+} from "@seanchas116/paintkit/src/components/sidebar/Inspector";
+import { SeparateComboBox } from "@seanchas116/paintkit/src/components/ComboBox";
+import { ColorInput } from "@seanchas116/paintkit/src/components/css/ColorInput";
+import { Icon } from "@iconify/react";
+import { LetterIcon } from "@seanchas116/paintkit/src/components/Input";
 
 function stringifyValue(value: TailwindValue | undefined) {
   if (!value) {
@@ -26,17 +35,50 @@ export const StyleInspector: React.FC = observer(() => {
   const fontWeight = sameOrNone(styles.map((s) => s.fontWeight));
 
   return (
-    <dl>
-      <dt className="text-macaron-disabledText">Width</dt>
-      <dd>{stringifyValue(width)}</dd>
-      <dt className="text-macaron-disabledText">Height</dt>
-      <dd>{stringifyValue(height)}</dd>
-      <dt className="text-macaron-disabledText">Color</dt>
-      <dd>{stringifyValue(color)}</dd>
-      <dt className="text-macaron-disabledText">Font Size</dt>
-      <dd>{stringifyValue(fontSize)}</dd>
-      <dt className="text-macaron-disabledText">Font Weight</dt>
-      <dd>{stringifyValue(fontWeight)}</dd>
-    </dl>
+    <>
+      <Pane>
+        <PaneHeadingRow>
+          <PaneHeading>Size</PaneHeading>
+        </PaneHeadingRow>
+        <div className="grid grid-cols-2 gap-2">
+          <SeparateComboBox
+            icon={<LetterIcon>W</LetterIcon>}
+            value={width?.value}
+          />
+          <SeparateComboBox
+            icon={<LetterIcon>H</LetterIcon>}
+            value={height?.value}
+          />
+        </div>
+      </Pane>
+      <Pane>
+        <PaneHeadingRow>
+          <PaneHeading>Text</PaneHeading>
+        </PaneHeadingRow>
+        <ColorInput value={color?.value} />
+        <div className="grid grid-cols-2 gap-2">
+          <SeparateComboBox
+            icon={<Icon icon="ic:outline-format-size" />}
+            value={fontSize?.value}
+          />
+          <SeparateComboBox
+            icon={<Icon icon="ic:outline-line-weight" />}
+            value={fontWeight?.value}
+          />
+        </div>
+      </Pane>
+      <dl className="p-2">
+        <dt className="text-macaron-disabledText">Width</dt>
+        <dd>{stringifyValue(width)}</dd>
+        <dt className="text-macaron-disabledText">Height</dt>
+        <dd>{stringifyValue(height)}</dd>
+        <dt className="text-macaron-disabledText">Color</dt>
+        <dd>{stringifyValue(color)}</dd>
+        <dt className="text-macaron-disabledText">Font Size</dt>
+        <dd>{stringifyValue(fontSize)}</dd>
+        <dt className="text-macaron-disabledText">Font Weight</dt>
+        <dd>{stringifyValue(fontWeight)}</dd>
+      </dl>
+    </>
   );
 });
