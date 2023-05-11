dd04dc18cfc58820f9977dcb6ea3509026fcaab8
Ryohei Ikegami <iofg2100@gmail.com>
2023-05-11 22:51:39 +0900

    Show each value

--- packages/editor/src/views/inspector2/StyleInspector.tsx
+++ packages/editor/src/views/inspector2/StyleInspector.tsx
@@ -6,5 +6,18 @@ export const StyleInspector: React.FC = observer(() => {
   const styles = appState.tailwindStyles;
   console.log(styles.map((s) => s.height));
 
-  return <div>{sameOrNone(styles.map((s) => s.color?.value))}</div>;
+  const color = sameOrNone(styles.map((s) => s.color?.value));
+  const width = sameOrNone(styles.map((s) => s.width));
+  const height = sameOrNone(styles.map((s) => s.height));
+
+  return (
+    <dl>
+      <dt className="text-macaron-disabledText">Color</dt>
+      <dd>{color}</dd>
+      <dt className="text-macaron-disabledText">Width</dt>
+      <dd>{width}</dd>
+      <dt className="text-macaron-disabledText">Height</dt>
+      <dd>{height}</dd>
+    </dl>
+  );
 });
