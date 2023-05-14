4bf67f8f1cfc7daf9e4037a3d492b813bc0e4bb8
Ryohei Ikegami <iofg2100@gmail.com>
2023-05-14 11:35:37 +0900

    Revert "Simpler size pane"
    This reverts commit 21e4124ca90fc76c79a8a68c32d7b69f6e9ab78d.

--- packages/editor/src/views/inspector/SizePane.tsx
+++ packages/editor/src/views/inspector/SizePane.tsx
@@ -1,13 +1,6 @@
 import { observer } from "mobx-react-lite";
 import { appState } from "../../state/AppState";
-import {
-  heights,
-  maxHeights,
-  maxWidths,
-  minHeights,
-  minWidths,
-  widths,
-} from "../../models/style/TailwindStyle";
+import { heights, widths } from "../../models/style/TailwindStyle";
 import {
   Pane,
   PaneHeading,
@@ -82,35 +75,15 @@ export const SizePane: React.FC = observer(() => {
               property="width"
               tokens={widths}
             />
+            <IconRadio options={horizontalSizeConstraintOptions} />
+          </Row11>
+          <Row11>
             <StyleComboBox
               icon={<LetterIcon>H</LetterIcon>}
               property="height"
               tokens={heights}
             />
-          </Row11>
-          <Row11>
-            <StyleComboBox
-              icon={<LetterIcon>{">"}</LetterIcon>}
-              property="minWidth"
-              tokens={minWidths}
-            />
-            <StyleComboBox
-              icon={<LetterIcon>{">"}</LetterIcon>}
-              property="minHeight"
-              tokens={minHeights}
-            />
-          </Row11>
-          <Row11>
-            <StyleComboBox
-              icon={<LetterIcon>{"<"}</LetterIcon>}
-              property="maxWidth"
-              tokens={maxWidths}
-            />
-            <StyleComboBox
-              icon={<LetterIcon>{"<"}</LetterIcon>}
-              property="maxHeight"
-              tokens={maxHeights}
-            />
+            <IconRadio options={verticalSizeConstraintOptions} />
           </Row11>
         </>
       )}
