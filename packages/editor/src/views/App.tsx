3a786b97f58dbe0b5d89348de292c1266dafaefa
Ryohei Ikegami <iofg2100@gmail.com>
2023-05-14 13:30:14 +0900

    Tweak width

--- packages/editor/src/views/App.tsx
+++ packages/editor/src/views/App.tsx
@@ -5,6 +5,8 @@ import { ScrollArea } from "@seanchas116/paintkit/src/components/ScrollArea";
 import { Outline } from "./outline/Outline";
 import { Viewport } from "./viewport/Viewport";
 
+// 221px = 100px * 2 (rows) + 8px * 2 (padding) + 4px (gap) + 1px (border)
+
 const App: React.FC = observer(() => {
   return (
     <PaintkitRoot colorScheme="dark">
@@ -13,11 +15,11 @@ const App: React.FC = observer(() => {
     fixed inset-0 w-screen h-screen flex text-xs
     "
       >
-        <aside className="w-[224px] h-full bg-macaron-uiBackground border-r border-macaron-separator relative contain-strict">
+        <aside className="w-[221px] h-full bg-macaron-uiBackground border-r border-macaron-separator relative contain-strict">
           <Outline />
         </aside>
         <Viewport />
-        <aside className="w-[224px] h-full bg-macaron-uiBackground border-l border-macaron-separator relative contain-strict">
+        <aside className="w-[221px] h-full bg-macaron-uiBackground border-l border-macaron-separator relative contain-strict">
           <ScrollArea>
             <StyleInspector />
           </ScrollArea>
