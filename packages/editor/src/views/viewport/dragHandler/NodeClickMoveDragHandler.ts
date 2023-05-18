9e904db23337a141744e542e4d4b34e8baba5274
Ryohei Ikegami <iofg2100@gmail.com>
2023-05-18 11:15:16 +0900

    createNodeClickMoveDragHandler

--- packages/editor/src/views/viewport/dragHandler/NodeClickMoveDragHandler.ts
+++ packages/editor/src/views/viewport/dragHandler/NodeClickMoveDragHandler.ts
@@ -1,63 +1,56 @@
 import { Vec2 } from "paintvec";
 import { DragHandler } from "./DragHandler";
-import { NodeMoveDragHandler } from "./NodeMoveDragHandler";
 import { dragStartThreshold } from "../constants";
 import { ViewportEvent } from "./ViewportEvent";
-import { Selectable } from "@uimix/model/src/models";
-import { projectState } from "../../../state/ProjectState";
+import { appState } from "../../../state/AppState";
+import { createNodeMoveDragHandler } from "./NodeMoveDragHandler";
 
-export class NodeClickMoveDragHandler implements DragHandler {
-  static create(event: ViewportEvent): NodeClickMoveDragHandler | undefined {
-    const selectable = event.selectable;
-    if (selectable) {
-      return new NodeClickMoveDragHandler(selectable, event);
-    }
+export async function createNodeClickMoveDragHandler(
+  event: ViewportEvent
+): Promise<DragHandler | undefined> {
+  const selectable = event.selectable;
+  if (!selectable) {
+    return;
   }
 
-  constructor(selectable: Selectable, event: ViewportEvent) {
-    this.initClientPos = new Vec2(event.event.clientX, event.event.clientY);
-    this.initPos = event.pos;
-    this.selectable = selectable;
-    this.additive = event.event.shiftKey;
+  const initClientPos = new Vec2(event.event.clientX, event.event.clientY);
+  const initPos = event.pos;
+  const additive = event.event.shiftKey;
+  let moveHandler: DragHandler | undefined;
 
-    if (event.selectables.every((s) => !s.ancestorSelected)) {
-      if (!this.additive) {
-        projectState.project.clearSelection();
+  if (event.selectables.every((s) => !s.ancestorSelected)) {
+    if (!additive) {
+      appState.document.deselectAll();
+    }
+    selectable.select();
+  }
+
+  return {
+    async move(event: ViewportEvent) {
+      if (!moveHandler) {
+        if (event.clientPos.sub(initClientPos).length < dragStartThreshold) {
+          return;
+        }
+
+        moveHandler = await createNodeMoveDragHandler(
+          event.artboard,
+          appState.document.selectedElements,
+          initPos
+        );
       }
-      this.selectable.select();
-    }
-  }
 
-  move(event: ViewportEvent): void {
-    if (!this.moveHandler) {
-      if (event.clientPos.sub(this.initClientPos).length < dragStartThreshold) {
-        return;
+      moveHandler?.move(event);
+    },
+
+    end(event: ViewportEvent): void {
+      moveHandler?.end(event);
+      if (!moveHandler) {
+        // do click
+        if (!additive) {
+          appState.document.deselectAll();
+        }
+        selectable.select();
       }
-
-      this.moveHandler = new NodeMoveDragHandler(
-        projectState.selectedSelectables,
-        this.initPos
-      );
-    }
-
-    this.moveHandler?.move(event);
-  }
-
-  end(event: ViewportEvent): void {
-    this.moveHandler?.end(event);
-    if (!this.moveHandler) {
-      // do click
-      if (!this.additive) {
-        projectState.project.clearSelection();
-      }
-      this.selectable.select();
-    }
-    projectState.undoManager.stopCapturing();
-  }
-
-  private readonly initPos: Vec2;
-  private readonly initClientPos: Vec2;
-  private readonly selectable: Selectable;
-  private readonly additive: boolean;
-  private moveHandler: DragHandler | undefined;
+    },
+  };
 }
