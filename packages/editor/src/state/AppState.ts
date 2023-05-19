23bd002c16a6a9b2cab5eeccaaf52a2f518a5ac5
Ryohei Ikegami <iofg2100@gmail.com>
2023-05-19 13:04:33 +0900

    Move hotkey handling to AppState

--- packages/editor/src/state/AppState.ts
+++ packages/editor/src/state/AppState.ts
@@ -9,8 +9,7 @@ import { Node, Document, FileNode, ElementNode } from "@windmix/model";
 import { ViewState } from "../types/ViewState";
 import { StyleInspectorTarget } from "../models/oldStyle/StyleInspectorTarget";
 import { Tool } from "./Tool";
-import { Rect } from "paintvec";
-import { DropDestination } from "./DropDestination";
+import hotkeys from "hotkeys-js";
 
 const vscode = acquireVsCodeApi();
 
@@ -124,6 +123,100 @@ export class AppState {
 }
 
 export const appState = new AppState();
+
+hotkeys(
+  "ctrl+z,command+z",
+  action((e) => {
+    e.stopPropagation();
+    appState.connection.rpc.remote.undo();
+  })
+);
+
+hotkeys(
+  "ctrl+shift+z,command+shift+z",
+  action((e) => {
+    e.stopPropagation();
+    appState.connection.rpc.remote.redo();
+  })
+);
+
+// zoom in
+hotkeys(
+  "ctrl+=,command+=",
+  action((e) => {
+    e.stopPropagation();
+    scrollState.zoomIn();
+  })
+);
+
+// zoom out
+hotkeys(
+  "ctrl+-,command+-",
+  action((e) => {
+    e.stopPropagation();
+    scrollState.zoomOut();
+  })
+);
+
+// insert mode
+hotkeys(
+  "esc",
+  action((e) => {
+    e.stopPropagation();
+    appState.tool = undefined;
+  })
+);
+hotkeys(
+  "t",
+  action((e) => {
+    e.stopPropagation();
+    appState.tool = {
+      type: "insert",
+      insertMode: "text",
+    };
+  })
+);
+hotkeys(
+  "f,r,b",
+  action((e) => {
+    e.stopPropagation();
+    appState.tool = {
+      type: "insert",
+      insertMode: "box",
+    };
+  })
+);
+
+hotkeys(
+  "backspace,delete",
+  action((e) => {
+    e.stopPropagation();
+    for (const selected of appState.document.selectedNodes) {
+      selected.remove();
+    }
+  })
+);
+
+hotkeys(
+  "space",
+  {
+    keydown: true,
+    keyup: true,
+  },
+  action((e) => {
+    if (e.type === "keydown") {
+      appState.panMode = true;
+    } else if (e.type === "keyup") {
+      appState.panMode = false;
+    }
+  })
+);
+
+window.addEventListener("beforeunload", () => {
+  // VSCode webviews should be reloaded explicitly when the iframe reloads
+  // (otherwise the black screen will be shown)
+  appState.connection.rpc.remote.reloadWebviews();
+});
 
 class NodeStyleInspectorTarget implements StyleInspectorTarget {
   constructor(element: ElementNode) {
