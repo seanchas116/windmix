f63d7e9ede260d6764219b003713c3634f07bb77
Ryohei Ikegami <iofg2100@gmail.com>
2023-05-26 19:47:32 +0900

    Add compound component example

---
+++ packages/example/src/Compound.tsx
@@ -0,0 +1,11 @@
+import Hero from "./Hero";
+import Pricing from "./Pricing";
+
+export default function Compound() {
+  return (
+    <>
+      <Hero navigation={[]} />
+      <Pricing />
+    </>
+  );
+}
